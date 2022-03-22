60dd6636485cba3e6496d0cd98f0188638a8148e
hsilv <hm.sebastiansilva@gmail.com>
2022-03-21 18:27:09 -0600

    Manejo de Maps de Inventario

--- src/common/Factory.java
+++ src/common/Factory.java
@@ -3,31 +3,32 @@ package common;
 import java.util.*;
 
 public class Factory {
-    public Factory(){
+
+    public Factory() {
     }
-    
+
     /**
      *
      * @param option
      * @param parametros
      * @return
      */
-    public static Map factoryMap(){
+    public static Map<String, String> factoryMap() {
         boolean flag = true;
         Scanner scan = new Scanner(System.in);
         System.out.println("Bienvenido, seleccione la interfaz que desea utilizar: \n1.HashMap \n2.TreeMap \n3.LinkedHashMap");
-        while(flag){
+        while (flag) {
             int option = scan.nextInt();
-            while(option <= 0 || option >=4){
+            while (option <= 0 || option >= 4) {
                 System.out.println("Ingrese una opcion valida");
                 option = scan.nextInt();
             }
-            if(option == 1){
-                return new HashMap<>(); 
-            } else if(option == 2){
+            if (option == 1) {
+                return new HashMap<>();
+            } else if (option == 2) {
                 return new TreeMap<>();
-            } else if(option == 3){
-                return new LinkedHashMap();
+            } else if (option == 3) {
+                return new LinkedHashMap<String, String>();
             }
         }
         return null;
--- src/common/HojaDeTrabajo6.java
+++ src/common/HojaDeTrabajo6.java
@@ -6,24 +6,38 @@ import java.util.*;
 public class HojaDeTrabajo6 {
 
     public static void main(String[] args) {
+        System.out.println("----------CREACION DE COLECCION DE USUARIO----------");
+        Map<String, String> userCollection = Factory.factoryMap();
+        System.out.println("---------------CREACION DE INVENTARIO---------------");
+        Map<String, String> inventory = Factory.factoryMap();
         ArrayList<String> inventario;
-        inventario = leerArchivo("F:\\Programacion\\HojaDeTrabajo6\\src\\resources\\ListadoProducto.txt");
-        Map<String, String> miMapa = Factory.factoryMap();
-
+        inventario = leerArchivo("/mnt/f/Programacion/HojaDeTrabajo6/src/resources/ListadoProducto.txt");
+        inventory = elaborarInventario(inventario, inventory);
+        System.out.println(inventory);
+        
     }
-
-    public static ArrayList<String> leerArchivo(String path) {
+    
+    public static ArrayList<String> leerArchivo(String path){
         ArrayList<String> array = new ArrayList<>();
-        try {
+        try{
             File doc = new File(path);
             Scanner obj = new Scanner(doc);
-            while (obj.hasNextLine()) {
+            while(obj.hasNextLine()){
                 array.add(obj.nextLine());
             }
-        } catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
         return array;
     }
 
+    public static Map<String, String> elaborarInventario(ArrayList<String> array, Map<String, String> mapa){
+        String[] strings;
+        for(String string: array){
+            strings = string.split("\\|");
+            mapa.put(strings[1].trim(), strings[0].trim());
+        }
+        return mapa;
+    }
+    
 }
